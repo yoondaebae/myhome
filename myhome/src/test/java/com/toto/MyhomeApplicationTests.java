ce226240f497662b781505c96de6f2583fe495ab
DESKTOP-TG2H36C\yoond <yoondaebae@gmail.com>
2022-04-30 22:10:45 +0900

    init

---
+++ myhome/src/test/java/com/toto/MyhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyhomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
