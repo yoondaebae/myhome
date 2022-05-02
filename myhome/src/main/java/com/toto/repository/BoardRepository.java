c47125e4c09fa8c16cb30f69f951c9cd100708d1
DESKTOP-TG2H36C\yoond <yoondaebae@gmail.com>
2022-05-02 23:09:18 +0900

    페이지 처리 및 검색

--- myhome/src/main/java/com/toto/repository/BoardRepository.java
+++ myhome/src/main/java/com/toto/repository/BoardRepository.java
@@ -2,6 +2,8 @@ package com.toto.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.toto.model.Board;
@@ -10,4 +12,6 @@ public interface BoardRepository extends JpaRepository<Board, Long>{
 
 	List<Board> findByTitle(String title);
 	List<Board> findByTitleOrContent(String title, String content);
+	
+	Page<Board> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
 }
--- myhome/src/main/java/com/toto/web/BoardController.java
+++ myhome/src/main/java/com/toto/web/BoardController.java
@@ -5,11 +5,14 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -32,9 +35,14 @@ public class BoardController {
 	private BoardValidator boardValidator;
 	
 	@GetMapping("/list")
-	public String list(Model model) {
+	public String list(Model model,@PageableDefault(size=2) Pageable pageable, @RequestParam(required = false, defaultValue = "") String searchText) {
 		
-		List<Board> boards = boardRepository.findAll();
+		//Page<Board> boards = boardRepository.findAll(pageable);
+		Page<Board> boards = boardRepository.findByTitleContainingOrContentContaining(searchText,searchText,pageable);
+		int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
+		int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4);
+		model.addAttribute("startPage", startPage);
+		model.addAttribute("endPage", endPage);
 		model.addAttribute("boards", boards);
 		return "board/list";
 	}
