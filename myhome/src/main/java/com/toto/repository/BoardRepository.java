5e21ec502d5702512d1e538cbf27ba8ff322d0d8
DESKTOP-TG2H36C\yoond <yoondaebae@gmail.com>
2022-05-02 21:23:12 +0900

    REST API 추가

--- myhome/src/main/java/com/toto/repository/BoardRepository.java
+++ myhome/src/main/java/com/toto/repository/BoardRepository.java
@@ -1,9 +1,13 @@
 package com.toto.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.toto.model.Board;
 
 public interface BoardRepository extends JpaRepository<Board, Long>{
 
+	List<Board> findByTitle(String title);
+	List<Board> findByTitleOrContent(String title, String content);
 }
